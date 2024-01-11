44e45260b42d204d05a4ea4693f7b19250ef1b1f
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2024-01-11 19:59:56 +0530

    remove Duplicate

---
+++ RemoveDuplicatesEasy.js
@@ -0,0 +1,21 @@
+array = [1,4,4,6,7,1]
+const uniqueArray=[...new Set(array)]
+console.log(uniqueArray)
+//////////////////////////////////////////////////
+const arr = [1,2,3,4,1,2];
+const b=[];
+for(let i=0;i<arr.length;i++){
+  if(b.indexOf(arr[i]) == -1){
+    b.push(arr[i])
+  }
+}
+console.log("removed array value",b)
+////////////////////////////////////////////////////
+const arr2 = [1,2,3,4,1,2];
+const c=[];
+arr.filter((dup)=>{
+  if(c.indexOf(arr[dup]) == -1){
+    c.push(arr[dup])
+  }
+})
+console.log("removed array value",c);
