6dd75c81283dd1ea9e2574adfef29e3e3fbea629
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2024-01-20 22:34:30 +0530

    Remove Numbers

--- removeNumbersFromString_3.js
+++ removeNumbersFromString_3.js
@@ -1,23 +1,25 @@
 function removeNumbersFromString(inputString, substringsToRemove) {
-    for (let i = 0; i < substringsToRemove.length; i++) {
-        inputString = inputString.split(substringsToRemove[i]).join('');
-    }
+  for (let i = 0; i < substringsToRemove.length; i++) {
+    inputString = inputString.split(substringsToRemove[i]).join('')
+  }
 
-    // Remove any remaining numbers
-    let resultString = '';
-    for (let i = 0; i < inputString.length; i++) {
-        if (isNaN(parseInt(inputString[i]))) {
-            resultString += inputString[i];
-        }
+  // Remove any remaining numbers
+  let resultString = ''
+  for (let i = 0; i < inputString.length; i++) {
+    //The isNaN(parseInt(inputString[i])) condition is used to check if the current character is not a number.
+    // If it's not a number, it's added to the resultString.
+    if (isNaN(parseInt(inputString[i]))) {
+      resultString += inputString[i]
     }
+  }
 
-    return resultString;
+  return resultString
 }
 
 // Example usage
-const inputString = "Hello123 World456!";
-const substringsToRemove = ["123", "456"];
+const inputString = 'Hello123 World456!'
+const substringsToRemove = ['123', '456']
 
-const result = removeNumbersFromString(inputString, substringsToRemove);
+const result = removeNumbersFromString(inputString, substringsToRemove)
 
-console.log(result);
+console.log(result)
