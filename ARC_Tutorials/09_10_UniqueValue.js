54a7ba0612b6f1ebfa3d5836580aabbebe2e6817
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2024-01-23 00:13:02 +0530

    JavaScript Coding Que

--- ARC_Tutorials/09_10_UniqueValue.js
+++ ARC_Tutorials/09_10_UniqueValue.js
@@ -1,11 +1,11 @@
-// How to find unique values in an array?
+//Q9. How to find unique values in an array?
 
 const Ids = [10,4,5,65,34,23,85,10,4,5];
 
 const uniqueValues = Ids.filter((element,index,array)=>array.indexOf(element)==index);
 console.log(uniqueValues);
 
-// How to find unique values from an Array in sorted order?
+//Q10. How to find unique values from an Array in sorted order?
 
 const sortedUniqueValue = uniqueValues.sort((a,b)=>a-b);
 console.log(sortedUniqueValue);
